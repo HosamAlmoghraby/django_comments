3a82efe1b3ca7e8e80849469f9ec4d253b315d7a
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-09-30 23:29:28 +0200

    create a new app 'comment'

--- comments/models.py
+++ comments/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
-from django.utils import timezone
-
-class Comment(models.Model):
-  title = models.CharField(max_length=20)
-  body = models.TextField()
-  date_added = models.DateTimeField(default=timezone.now)
-  
-  def __str__(self):
-    return self.title
-    
-  class Meta:
-    verbose_name_plural = "Comments"
+# Create your models here.
