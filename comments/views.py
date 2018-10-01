3518b9ef63ff93dfab3118010b4e5df406fb2563
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-10-01 23:47:44 +0200

    show list of comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Comment
+# from .forms import CommentForm
+
+
+def index(request):
+  comments = Comment.objects.all()
+  context = {
+    'comments': comments
+  }
+  return render(request, 'comments/index.html', context)
+
+
+# def submitForm(request):
+#   if request.method == 'POST':
+#     form = FormName(request.POST)
+    
+#     if form.is_valid():
+#       new_submit = Comment.objects.create(title=request.POST['title'], body=request.POST['body'])
+#       return redirect('index')
+      
+#     else:
+#       form = FormName()
+  
+#   context = {'form': form}
+#   return render(request, 'appName/submit.html', context)
